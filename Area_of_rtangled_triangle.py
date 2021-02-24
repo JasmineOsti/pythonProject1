7ea223fca4b48eb889c5b7625dbdffa7e137940e
Lenovo Thinkbook <jasmineosti03@gmail.com>
2021-02-24 11:25:26 +0545

    Area of right angled triangle

---
+++ Area_of_rtangled_triangle.py
@@ -0,0 +1,5 @@
+#Write a program that reads the length of the base and the height of a right angled triangle and prints the area. Every number is given on a separate line.
+Base= float(input("Enter the base"))
+Height= float(input("Enter the height"))
+Area= Base*Height/2
+print("Area is", Area)
