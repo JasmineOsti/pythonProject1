328887e87d10f5d918a31b2d52ad531862540f26
Lenovo Thinkbook <jasmineosti03@gmail.com>
2021-02-24 11:14:49 +0545

    PythonLab

---
+++ Program.py
@@ -0,0 +1,6 @@
+#Write a program that takes three numbers and prints their sum. Every number is given on a separate line.
+A = int(input("Enter the first number: "))
+B = int(input("Enter the second number: "))
+C = int(input("Enter the third number: "))
+sum = A+B+C
+print("the sum of three numbers is", sum)
---
+++ add.py
@@ -0,0 +1,34 @@
+#num=int(input('Enter a number:'))
+#if num<0:
+#    print('number is positive')
+#else:
+#    print('number is negative')
+#num=int(input('Enter a number:'))
+#if num<0:
+#   print('we have given negative number')
+#print('program is over')
+#num=int(input('enter a number'))
+#if num%2==0:
+#    print('it is an even number')
+#print('program is over')
+num=int(input('enter week day'))
+if num==1:
+    print('sunday')
+elif num==2:
+    print('monday')
+elif num==3:
+    print('tuesday')
+elif num==4:
+    print('wednesday')
+elif num==5:
+    print('thursday')
+elif num==6:
+    print('friday')
+elif num==7:
+    print('saturday')
+else:
+    print('it is invalid number')
+
+
+
+
