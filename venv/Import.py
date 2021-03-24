db7d0f9378bef8a01ae82cf4072c4e29da2d6b8e
Lenovo Thinkbook <jasmineosti03@gmail.com>
2021-03-24 12:04:24 +0545

    Amend

---
+++ venv/Import.py
@@ -0,0 +1,15 @@
+'''def sum(a,b):
+    c=a+b
+    #print(f"the sum of {a} and {b} is {c}")
+    return c
+a=int(input("enter first number:"))
+b = int(input("enter second number:"))
+print(sum(a,b))
+'''
+def sum():
+    a=int(input("Enter a number:"))
+    b=int(input("Enter 2nd number:"))
+    c=a+b
+    return c
+
+print(sum())
